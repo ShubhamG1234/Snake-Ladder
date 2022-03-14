0cf68ac2c5f64e247ef3b72b1b1b13b7738dc4ba
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-03-14 12:12:31 +0530

    initial commit

---
+++ UC2_DieRolls/UC2_DieRolls/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Snake And Ladder");
+{
+    Random random = new Random();
+    int CheckDie = random.Next(1, 7);
+    Console.WriteLine("Die number:" + CheckDie);
+}
